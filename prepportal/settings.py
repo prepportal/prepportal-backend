ca04a491fc25df834ef82881eeafbb482e40732b
Akash R Chandran <chandranrakash@gmail.com>
2023-08-28 20:28:13 +0530

    updated settings

--- prepportal/settings.py
+++ prepportal/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*lau3ea78+)lm9ey#p#dpnih6_n67p*twei&$@vg34c3l*f4s9'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*", "chrome-extension://amknoiejhlmhancpahfcfcfhllgkpbld"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -130,6 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
