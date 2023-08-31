ea7080e25fe2c290f21aaa792e0a3c11a3c7ceec
Akash R Chandran <chandranrakash@gmail.com>
2023-09-01 00:04:12 +0530

    feat: created api to get subjects using branch id

---
+++ api/subject/subject_urls.py
@@ -0,0 +1,8 @@
+from django.urls import include, path
+from api.subject.subject_views import SubjectAPI
+
+
+urlpatterns = [
+    path("", SubjectAPI.as_view()),
+    path("<str:branch_id>/", SubjectAPI.as_view()),
+]
---
+++ api/subject/subject_views.py
@@ -0,0 +1,34 @@
+from rest_framework.views import APIView
+from api.models import Subject
+from api.subject.subject_serializers import SubjectSerializer
+from util.response import CustomResponse
+from util.pagination import get_paginated_queryset
+from rest_framework import status
+
+
+class SubjectAPI(APIView):
+    def get(self, request, branch_id=None):
+        try:
+            if branch_id:
+                if not (
+                    subjects := Subject.objects.filter(
+                        semester__branch_id=branch_id
+                    )
+                ):
+                    return CustomResponse(
+                        general_message="Subject Does Not Exists"
+                    ).get_failure_response(
+                        status_code=status.HTTP_404_NOT_FOUND,
+                        http_status_code=status.HTTP_404_NOT_FOUND,
+                    )
+                serializer = SubjectSerializer(subjects, many=True)
+                return CustomResponse(response=serializer.data).get_success_response()
+            else:
+                subjects = Subject.objects.all()
+                paginated_queryset = get_paginated_queryset(
+                subjects, request, ["name"], {"name": "name"}
+                )
+                serializer = SubjectSerializer(paginated_queryset.get("queryset"), many=True)
+                return CustomResponse().paginated_response(data=serializer.data, pagination=paginated_queryset.get("pagination"))
+        except Exception as e:
+            return CustomResponse(general_message=str(e)).get_failure_response()
