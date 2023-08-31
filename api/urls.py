78e1e92c0e5c893e25250afbd5a7d516528e3c1b
Akash R Chandran <chandranrakash@gmail.com>
2023-09-01 00:03:23 +0530

    changed settings

--- api/urls.py
+++ api/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path("semester-groups/", include("api.semester_group.semester_group_urls")),
     path("semesters/", include("api.semester.semester_urls")),
     path("subject-types/", include("api.subject_type.subject_type_urls")),
+    path("subjects/", include("api.subject.subject_urls")),
+
+    
     path("__debug__/", include(debug_toolbar.urls)),
 ]
--- prepportal/settings.py
+++ prepportal/settings.py
@@ -137,3 +137,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
