8eb4a101f989d3517a18d74436db4e530ebcc23f
Akash R Chandran <chandranrakash@gmail.com>
2023-09-01 01:28:30 +0530

    removed unwanted file

--- api/question_paper/question_papers_urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-from api.question_paper.question_paper_views import QuestionPaperAPI
-
-
-urlpatterns = [
-    path("", QuestionPaperAPI.as_view()),
-    path("<str:subject_id>/", QuestionPaperAPI.as_view()),
-]
