454c2d13387b382017f26785821c3caade9b20b0
Akash R Chandran <chandranrakash@gmail.com>
2023-09-13 00:54:07 +0530

    admin panel and removed unused urls

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from api.models import Branch, Semester, Subject, SubjectType, QuestionPaper, Note
 
 # Register your models here.
+admin.site.register(Branch)
+admin.site.register(Semester)
+admin.site.register(Subject)
+admin.site.register(SubjectType)
+admin.site.register(QuestionPaper)
+admin.site.register(Note)
--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("branches/", include('api.branch.branch_urls')),
-    path("semester-groups/", include("api.semester_group.semester_group_urls")),
     path("semesters/", include("api.semester.semester_urls")),
     path("subject-types/", include("api.subject_type.subject_type_urls")),
     path("subjects/", include("api.subject.subject_urls")),
