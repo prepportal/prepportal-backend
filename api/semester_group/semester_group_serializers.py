6dc00eaa09bca413eee135a6b15eab94af079a28
Akash R Chandran <chandranrakash@gmail.com>
2023-09-13 00:46:26 +0530

    Removed semester group models

--- api/semester_group/semester_group_serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from ..models import SemesterGroup
-
-
-class SemsterGroupSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SemesterGroup
-        fields = "__all__"
--- api/semester_group/semester_group_urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-from api.semester_group.semester_group_views import SemesterGroupAPI
-
-
-urlpatterns = [
-    path("", SemesterGroupAPI.as_view()),
-    path("<str:semester_group_id>/", SemesterGroupAPI.as_view()),
-]
