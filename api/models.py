b1cce79699d14b05c18230fc17d46e70f8653a10
Akash R Chandran <chandranrakash@gmail.com>
2023-09-13 10:00:14 +0530

    changed models.py

--- api/models.py
+++ api/models.py
@@ -43,9 +43,7 @@ class Subject(models.Model):
     subject_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     code = models.CharField(max_length=20)
-    branch_code = models.CharField(max_length=6)
     branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
-    semester_code = models.CharField(max_length=6)
     semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
     subject_type = models.ForeignKey(SubjectType, on_delete=models.CASCADE)
     description = models.TextField(null=True, blank=True)
@@ -59,17 +57,13 @@ class Subject(models.Model):
 class QuestionPaper(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
-    branch_code = models.CharField(max_length=6)
-    branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
-    semester_code = models.CharField(max_length=6)
-    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
     month = models.CharField(max_length=20)
     year = models.IntegerField()
     file_id = models.CharField(max_length=255)
     file_size = models.IntegerField()
     file_format = models.CharField(max_length=20)
     thumb = models.URLField()
-    subject_code = models.CharField(max_length=6)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
     description = models.TextField(null=True, blank=True)
 
     def __str__(self):
@@ -80,11 +74,7 @@ class QuestionPaper(models.Model):
 
 class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=255)
-    branch_code = models.CharField(max_length=6)
-    branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
-    semester_code = models.CharField(max_length=6)
-    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
     file_id = models.CharField(max_length=255)
     file_size = models.IntegerField()
     file_format = models.CharField(max_length=20)
